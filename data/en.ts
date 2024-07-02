f61484e4d2a65071f5f0f717a1ec4d6adeb82358
Ryota Kato <rka147@sfu.ca>
2024-07-01 17:30:59 -0700

    retry deployment

--- data/en.ts
+++ data/en.ts
@@ -6,58 +6,58 @@ const data: Data = {
 
     projects: [
         {
-        title: 'Portfolio',
-        techStack: [
-            'React',
-            'Next.js',
-            'TailwindCSS',
-            'TypeScript',
-            'Vercel',
-            'Shadcn/ui',
-        ],
-        // description: 'An environmental impact tracking app for individuals and small businesses',
-        image: '/public/photograph/pic1.jpg',
+            title: 'Portfolio',
+            techStack: [
+                'React',
+                'Next.js',
+                'TailwindCSS',
+                'TypeScript',
+                'Vercel',
+                'Shadcn/ui',
+            ],
+            // description: 'An environmental impact tracking app for individuals and small businesses',
+            image: '/public/photograph/pic1.jpg',
         },
         {
-        title: 'Portfolio-1',
-        techStack: [
-            'Java',
-            'Kotlin',
-            'Android',
-            'Java',
-            'Kotlin',
-            'Android',
-        ],
-        // description: 'An environmental impact tracking app for individuals and small businesses',
-        image: '/public/photograph/pic2.jpg',
+            title: 'Portfolio-1',
+            techStack: [
+                'Java',
+                'Kotlin',
+                'Android',
+                'Java',
+                'Kotlin',
+                'Android',
+            ],
+            // description: 'An environmental impact tracking app for individuals and small businesses',
+            image: '/public/photograph/pic2.jpg',
         }
     ],
     
     projectDialog: [
         {
-        title: 'Portfolio',
-        description:
-            'An environmental impact tracking app for individuals and small businesses',
-        techStack: [
-            'React',
-            'Next.js',
-            'TailwindCSS',
-            'TypeScript',
-            'Vercel',
-            'Shadcn/ui',
+                title: 'Portfolio',
+                description:
+                    'An environmental impact tracking app for individuals and small businesses',
+                techStack: [
+                    'React',
+                    'Next.js',
+                    'TailwindCSS',
+                    'TypeScript',
+                    'Vercel',
+                    'Shadcn/ui',
         ],
         },
         {
-        title: 'Portfolio-1',
-        description:
-            'An environmental impact tracking app for individuals and small businesses',
-        techStack: [
-            'Java',
-            'Kotlin',
-            'Android',
-            'Java',
-            'Kotlin',
-            'Android',
+            title: 'Portfolio-1',
+            description:
+                'An environmental impact tracking app for individuals and small businesses',
+            techStack: [
+                'Java',
+                'Kotlin',
+                'Android',
+                'Java',
+                'Kotlin',
+                'Android',
         ],
         }
     ],
